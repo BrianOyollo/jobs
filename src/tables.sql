15102be07c22bfeff87557d5296964dbe27d8a81
oyollobrian <oyollobrian@gmail.com>
2025-09-13 02:21:02 +0300

    add reminders table

--- src/tables.sql
+++ src/tables.sql
@@ -7,5 +7,13 @@ CREATE TABLE jobs (
     published TIMESTAMP NULL,
     author TEXT,
     category TEXT NOT NULL,
-    fetched_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    fetched_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    application_status TEXT DEFAULT 'pending',  -- pending | applied | skipped
+    reason TEXT
+);
+
+CREATE TABLE reminders (
+    id SERIAL PRIMARY KEY,
+    job_id INT REFERENCES jobs(id) ON DELETE CASCADE,
+    sent_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
