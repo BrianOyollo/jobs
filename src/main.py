1a9cddf60693cc43b773b690928f5e3690e3d538
oyollobrian <oyollobrian@gmail.com>
2025-09-15 13:02:07 +0300

    updated reliefweb link

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ def main():
     cur = conn.cursor()
 
     # ReliefWeb
-    rss_feedlink = "https://reliefweb.int/jobs?advanced-search=%28CC6866%29_%28C131%29"
+    rss_feedlink = "https://reliefweb.int/jobs/rss.xml?advanced-search=%28CC6866%29_%28C131%29"
     # rss_feedlink="/home/brianoyollo/Downloads/rss(1).xml"
     jobs = reliefweb.extract_jobs(rss_feedlink)
     utils.insert_jobs(conn, cur, jobs, notification_title='Jobs from Relief Web')
